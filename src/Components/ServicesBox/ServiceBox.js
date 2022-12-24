3e6dae2e70fe9f38ec93e6df1db0e42b5990e6a3
masoud <masoud.red64@gamil.com>
2022-12-25 00:28:19 +0330

    change 2

--- src/Components/ServicesBox/ServiceBox.js
+++ src/Components/ServicesBox/ServiceBox.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./ServiceBox.css";
-// import Jump from "react-reveal/Jump";
+import Jump from "react-reveal/Jump";
 export default function ServiceBox({ img, title, text }) {
   return (
-    // <Jump delay={1000} duration={2000}>
-    <div className="service-box">
-      <div className="img-service-box">
+    <Jump delay={1000} duration={2000}>
+      <div className="service-box">
         <img src={img} alt="" />
+
+        <h1 className="title-service-box">{title}</h1>
+        <p className="text-service-box">{text} </p>
       </div>
-      <h1 className="title-service-box">{title}</h1>
-      <p className="text-service-box">{text} </p>
-    </div>
-    // </Jump>
+    </Jump>
   );
 }
