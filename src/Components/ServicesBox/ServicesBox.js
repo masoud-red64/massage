f7b24f6dc8ba1927f28b469dfbc8ebbb196e19b2
masoud <masoud.red64@gamil.com>
2022-12-25 01:20:15 +0330

    change 2

--- src/Components/ServicesBox/ServicesBox.js
+++ src/Components/ServicesBox/ServicesBox.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./ServicesBox.css";
 import { Container, Row, Col } from "react-bootstrap";
 import ServiceBox from "./ServiceBox";
+import servicebox1 from "../../photos/servicebox-1.jpg";
+import servicebox2 from "../../photos/servicebox-2.jpg";
+import servicebox3 from "../../photos/servicebox-3.jpg";
 
 export default function ServicesBox() {
   return (
@@ -10,7 +13,7 @@ export default function ServicesBox() {
         <Row>
           <Col xxl={3} xl={3} md={6} sm={12}>
             <ServiceBox
-              img={"./images/serviceBox-1.jpg"}
+              img={servicebox1}
               title={"ماساژ سر و صورت"}
               text={
                 "از ماساژ سر و صورت برای آرامش استفاده‌ های بسیاری می‌شود. عموما افراد زیادی در هنگام ماساژ از ماساژور می‌خواهند که زمان زیادی را روی صورتشان بگذراند. این دلیل بیشتر از موارد زیبایی که ماساژ صورت به همراه خود دارد، بیشتر به دلیل آرامش خاص آن است."
@@ -19,7 +22,7 @@ export default function ServicesBox() {
           </Col>
           <Col xxl={3} xl={3} md={6} sm={12}>
             <ServiceBox
-              img={"./images/serviceBox-2.jpg"}
+              img={servicebox2}
               title={"ماساژ تخصصی"}
               text={
                 "ماساژ تخصصی عبارتست از دست ورزی بر روی بافتهای نرم بدن. این ماساژ به رهایی از استرس و فشار عضلانی و درد ناشی از صدمات و سرعت بهبود ناراحتیهای حاد و مزمن كمك می كند.از ماساژ تخصصی می توان به ماساژ سنگ داغ، ماساژ با شمع و …اشاره کرد."
@@ -28,7 +31,7 @@ export default function ServicesBox() {
           </Col>
           <Col xxl={3} xl={3} md={8} sm={12}>
             <ServiceBox
-              img={"./images/serviceBox-3.jpg"}
+              img={servicebox3}
               title={"ماساژ بدن"}
               text={
                 "هدف اصلی ماساژ بدن، کمک به بهبود بدن جهت افزایش سلامتی و آرامش است. این به معنای متعادل سازی نیازهای روانی، عاطفی و فیزیکی فرد است.انواع تکنیک و سبک ماساژ بر اساس مهارت، دامنه تمرین و هدف اصلی جلسه ماساژ تعیین می‌شود."
