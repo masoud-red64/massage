3051ce3dd163bdbd03dac9b9d9934f1ccc2c42e0
masoud <masoud.red64@gamil.com>
2022-12-30 16:47:45 +0330

    add opinion section with logic and backend

---
+++ src/Components/Opinion/Opinion.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef, useState } from "react";
+import { supabase } from "../../supabaseClient";
+import "./Opinion.css";
+
+export default function Opinion(props) {
+  const [isShowAnswerInput, setIsShowAnswerInput] = useState(false);
+  const [answer, setAnswer] = useState("");
+  const [disableAnswer, setDisableAnswer] = useState(false);
+
+  const suggest = props.suggest;
+
+  const input = useRef();
+  const answerBtn = useRef();
+
+  const sendOpinion = async (id) => {
+    try {
+      const { data, error } = await supabase
+        .from("suggests")
+
+        .update({ answer: answer })
+        .eq("id", id);
+
+      if (error) throw error;
+      if (data != null) {
+        console.log(data);
+      }
+    } catch (error) {
+      alert(error);
+    }
+
+    setIsShowAnswerInput(false);
+    setDisableAnswer(true);
+  };
+
+  //   useEffect(() => {
+  //     showAnswerInput()
+  // }, [isShowAnswerInput]);
+
+  const showAnswerInput = async () => {
+    await setIsShowAnswerInput(true);
+    input.current.focus();
+  };
+
+  return (
+    <>
+      {suggest.accept && (
+        <div className="opinion">
+          <div className="header-opinion">
+            <div className="first-last-name-opinion">
+              <h3 className="firstname-opinion">{suggest.firstname}</h3>
+              <h3 className="lastname-opinion">{suggest.lastname}</h3>
+            </div>
+            <p className="date-opinion">{suggest.date}</p>
+            <div className="profile-opinion">
+              <img src="./images/concat.jpg" alt="" />
+            </div>
+          </div>
+          <p className="message-opinion">{suggest.opinion}</p>
+          <div className="footer-opinion">
+            {isShowAnswerInput ? (
+              <button
+                className="send-btn-opinion"
+                onClick={() => sendOpinion(suggest.id)}
+              >
+                ارسال
+              </button>
+            ) : (
+              <button
+                ref={answerBtn}
+                className="btn-opinion"
+                onClick={showAnswerInput}
+                disabled={disableAnswer}
+              >
+                پاسخ
+              </button>
+            )}
+          </div>
+          {suggest.acceptAnswer && (
+            <p className="answer-opinion-message">
+              <span>ادمین: </span>
+              {suggest.answer}
+            </p>
+          )}
+          {isShowAnswerInput && (
+            <input
+              ref={input}
+              type="text"
+              className="answer-opinion-input"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+          )}
+        </div>
+      )}
+    </>
+  );
+}
--- src/Components/Suggest/Suggest.js
+++ src/Components/Suggest/Suggest.js
@@ -1,9 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Suggest.css";
 import { Rotate } from "react-reveal";
+import { supabase } from "../../supabaseClient";
+import Opinion from "../Opinion/Opinion";
 
 export default function Suggest() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [opinion, setOpinion] = useState("");
+  const [suggests, setSuggests] = useState([]);
+
+  let currentdate = new Date();
+  let date = new Date().toLocaleDateString("fa-IR");
+  let time = `${currentdate.getHours()}:${
+    currentdate.getMinutes() < 10
+      ? "0" + currentdate.getMinutes()
+      : currentdate.getMinutes()
+  }:${
+    currentdate.getSeconds() < 10
+      ? "0" + currentdate.getSeconds()
+      : currentdate.getSeconds()
+  }  -  ${date}`;
+
+  useEffect(() => {
+    getSuggest();
+    console.log(time);
+  }, []);
+
+  async function getSuggest() {
+    try {
+      const { data, error } = await supabase
+        .from("suggests")
+        .select("*")
+        .limit(10);
+
+      if (error) throw error;
+
+      if (data != null) {
+        setSuggests(data);
+        console.log(data);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  async function createSuggest(event) {
+    event.preventDefault();
+    let newSuggest = {
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      opinion: opinion,
+      date: time,
+    };
+
+    if (firstname && lastname && email && opinion) {
+      try {
+        const { data, error } = await supabase
+          .from("suggests")
+          .insert(newSuggest)
+          .single();
+
+        if (error) throw error;
+        if (data != null) {
+          console.log(data);
+        }
+      } catch (error) {
+        alert(error);
+      }
+    }
+  }
+
   return (
     <Container fluid className="suggest">
       <Row>
@@ -20,12 +90,16 @@ export default function Suggest() {
                   className="name"
                   placeholder="نام *"
                   required
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
                 />
                 <input
                   type="text"
                   className="lastname"
                   placeholder="نام خانوادگی *"
                   required
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
                 />
               </div>
               <div className="site-email">
@@ -34,6 +108,8 @@ export default function Suggest() {
                   className="name"
                   placeholder="ایمیل *"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <input type="text" className="lastname" placeholder="وب سایت" />
               </div>
@@ -44,11 +120,16 @@ export default function Suggest() {
                 rows="10"
                 placeholder="پیام *"
                 required
+                value={opinion}
+                onChange={(e) => setOpinion(e.target.value)}
               ></textarea>
-              <button className="send" type="submit">
+              <button className="send" type="submit" onClick={createSuggest}>
                 ارسال
               </button>
             </form>
+            {suggests.reverse().map((suggest) => (
+              <Opinion suggest={suggest} />
+            ))}
           </Rotate>
         </Col>
         <Col xxl={4} xl={4} md={12} sm={12} className="last-img">
