ecf402bed7889e91635faa9c7761387deed787ad
masoud <masoud.red64@gamil.com>
2023-03-20 20:03:28 +0330

    loading in gallery page

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./Gallery.css";
 import { staticPictures, dynamicPictures } from "../../data";
 
 export default function Gallery() {
   const [showMore, setShowMore] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    window.addEventListener("load", () => setLoading(false));
+    setLoading(false);
+  }, [location]);
+
   return (
     <>
       <div className="gallery">
+        {loading && (
+          <div class="loading">
+            درحال دریافت اطلاعات
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </div>
+        )}
         {staticPictures.map((staticPicture) => (
           <div key={staticPicture.id} className={`img-${staticPicture.id}`}>
             <img loading="lazy" src={staticPicture.img} alt="" />
