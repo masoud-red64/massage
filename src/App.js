ca8408accac1efb6e4a6946d5ca5869d339966af
masoud <masoud.red64@gamil.com>
2023-03-21 13:22:41 +0330

    set loading for App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import "./App.css";
 
 import routse from "./Components/routes";
 import { useRoutes } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
   const router = useRoutes(routse);
+  const [loading, setLoading] = useState(true)
 
-  return <div className="App">{router}</div>;
+  useEffect(()=>{
+    window.addEventListener('load', setLoading(false))
+  },[])
+
+  return <div className="App">
+    {loading ? (
+          <div class="loading">
+            درحال دریافت اطلاعات
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </div>
+        )
+      :router
+      }
+</div>;
 }
 
 export default App;
