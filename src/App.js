399d917eae22ae66117396a6b14a5e907a0bda86
masoud <masoud.red64@gamil.com>
2023-03-24 23:19:47 +0430

    add Suspense for loading

--- src/App.js
+++ src/App.js
@@ -2,29 +2,27 @@ import "./App.css";
 
 import routse from "./Components/routes";
 import { useRoutes } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, Suspense } from "react";
 
 function App() {
   const router = useRoutes(routse);
-  const [loading, setLoading] = useState(true)
 
-  useEffect(()=>{
-    window.addEventListener('load', setLoading(false))
-  },[])
-
-  return <div className="App">
-    {loading ? (
+  return (
+    <div className="App">
+      <Suspense
+        fallback={
           <div class="loading">
             درحال دریافت اطلاعات
             <span>.</span>
             <span>.</span>
             <span>.</span>
           </div>
-        )
-      :router
-      }
-</div>;
+        }
+      >
+        {router}
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/routes.js
+++ src/Components/routes.js
@@ -1,13 +1,24 @@
-import ContactUs from "./ContactUs/ContactUs";
-import Navbar from "./NavBar/Navbar";
-import Header from "./Header/Header";
-import Services from "./Services/Services";
-import Aboutus from "./Aboutus/Aboutus";
-import ServicesBox from "./ServicesBox/ServicesBox";
-import Suggest from "./Suggest/Suggest";
-import Footer from "./Footer/Footer";
-import TurnRating from "./TurnRating/TurnRating";
-import Gallery from "./Gallery/Gallery";
+// import ContactUs from "./ContactUs/ContactUs";
+// import Navbar from "./NavBar/Navbar";
+// import Header from "./Header/Header";
+// import Services from "./Services/Services";
+// import Aboutus from "./Aboutus/Aboutus";
+// import ServicesBox from "./ServicesBox/ServicesBox";
+// import Suggest from "./Suggest/Suggest";
+// import Footer from "./Footer/Footer";
+// import TurnRating from "./TurnRating/TurnRating";
+// import Gallery from "./Gallery/Gallery";
+import React from "react";
+const Gallery = React.lazy(() => import("./Gallery/Gallery"));
+const ContactUs = React.lazy(() => import("./ContactUs/ContactUs"));
+const Navbar = React.lazy(() => import("./NavBar/Navbar"));
+const Header = React.lazy(() => import("./Header/Header"));
+const Services = React.lazy(() => import("./Services/Services"));
+const Aboutus = React.lazy(() => import("./Aboutus/Aboutus"));
+const ServicesBox = React.lazy(() => import("./ServicesBox/ServicesBox"));
+const Suggest = React.lazy(() => import("./Suggest/Suggest"));
+const Footer = React.lazy(() => import("./Footer/Footer"));
+const TurnRating = React.lazy(() => import("./TurnRating/TurnRating"));
 
 const topScroll = () => {
   window.scrollTo(0, 0);
